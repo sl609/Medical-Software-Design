84df79d938a9535e62767eb549450970d74287d5
sl609 <sl609@duke.edu>
2021-01-28 16:17:33 -0500

    add interface

---
+++ interface.py
@@ -0,0 +1,11 @@
+def interface():
+	print("Blood test Analysis")
+	while True:
+		print("Options")
+		print("9 - Quits")
+		choice = input("Enter an options: ")
+		if choice == "9":
+			return
+
+interface()
+
